0e350574defe65f56c47496eda575687ec7e8e1e
alumno <alumno@pc18.iris.majuelo.es>
2020-01-27 20:33:05 +0100

    Arreglado suma de partidas y visual

--- juego.php
+++ juego.php
@@ -3,8 +3,43 @@ session_start();
 	if (!isset($_POST["Comprobar"])){
 		$_POST["Vida"]=5;
 
+		$myfile = fopen("Usuarios.txt", "r") or die("Unable to open file!");
+		$linea=0;
+		$texto="";
+		$texto.=fgets($myfile);//saltar primer separador
+		while(!feof($myfile)){
+			$nombre=fgets($myfile);//conseguir nombre
+			$apellidos=fgets($myfile);//conseguir apppelidos
+			$texto.=$nombre;
+			$texto.=$apellidos;
+			if ($_GET["nom"]== trim($nombre)){
+				$_SESSION["Nombre"]=$nombre;
+				$_SESSION["Linea"]=$linea;
+				$texto.=fgets($myfile);//saltar correo
+				$texto.=fgets($myfile);//guardar fecha
+				$texto.=((int)fgets($myfile)+1)."\n";//saltar partidas jugadas
+				$texto.=fgets($myfile);//saltar partidas ganadas
+				$texto.=fgets($myfile);//saltar linea (separador)
+			}else{
+				$texto.=fgets($myfile);//saltar correo
+				$texto.=fgets($myfile);//guardar fecha
+				$texto.=fgets($myfile);//saltar partidas jugadas
+				$texto.=fgets($myfile);//saltar partidas ganadas
+				$texto.=fgets($myfile);//saltar linea (separador)
+				$linea=$linea+7;//guarda la line desde donde empieza el usuario
+			}
+			
+		}
+		fclose($myfile);
+		file_put_contents("Usuarios.txt", $texto);
+		
+		$myfile = fopen("Palabras.txt", "r") or die("Unable to open file!");
+		//FFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+
+
 		$palabraelegida="abcde";
 		$palabrablank="_____";
+
 
 		$_SESSION["PALABRA"]=$palabraelegida;
 		$_SESSION["BLANK"]=$palabrablank;
@@ -15,7 +50,7 @@ session_start();
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title id="title">Inicio</title>
+        <title id="title">Ahorcado</title>
         <link rel="stylesheet" href="Style/styles.css" />
         <link rel="Shortcut Icon" href="Imagenes/icono.png">
         <script type="text/javascript" src="Scripts/javaInicio.js"></script>
@@ -31,50 +66,84 @@ session_start();
 					<?php
 						$myfile = fopen("Ahorcado.txt", "r") or die("Unable to open file!");
 						
-						for($i=0;$i<(5-(int)$_POST["Vida"])*7;$i++)
-							fgets($myfile); //saltar dibujos si es nesesario
-						for($i=0;$i<7;$i++)
-							echo fgets($myfile); //7 lineas del dibujo
-					?>
-				</label>
-				<br><label id="palabra"class="datos">
-					<?php
-						if (!isset($_POST["Comprobar"]))
-							echo $_SESSION["BLANK"];
-						else{
+						if (!isset($_POST["Comprobar"])){
+							for($i=0;$i<7;$i++)
+								echo fgets($myfile); //7 lineas del dibujo
+							fclose($myfile);
+						}else{
 							$pos = strrpos($_SESSION["PALABRA"], $_POST["Letra"]);
+
 							if ($pos === false)
 								$_POST["Vida"]=((int)$_POST["Vida"]-1);
 							else{
 								$encontrado=false;
 								$max=strlen($_SESSION["PALABRA"]);
 								for ($i=0;$i<$max;$i++){
-									if ($_SESSION["PALABRA"][$i]==$_POST["Letra"]){
+									if ($_SESSION["PALABRA"][$i]==$_POST["Letra"])
 										if ($_SESSION["BLANK"][$i]!=$_POST["Letra"])
 											$_SESSION["BLANK"][$i]=$_POST["Letra"];
 										else
 											$encontrado=true;
-									}
+									
 								}
 								if ($encontrado)
 									$_POST["Vida"]=((int)$_POST["Vida"]-1);
 							}
-							echo $_SESSION["BLANK"];
+							for($i=0;$i<(5-(int)$_POST["Vida"])*7;$i++)
+								fgets($myfile); //saltar dibujos si es nesesario
+							for($i=0;$i<7;$i++)
+								echo fgets($myfile); //7 lineas del dibujo
+							fclose($myfile);
 						}
-
 					?>
 				</label>
-				<br><br><label class="datos" >Letra:</label>
-				<select class="datos" name="Letra">
-					<?php
+				<br><label id="palabra"class="datos"><?php echo $_SESSION["BLANK"];?></label>
+				<?php
+					if ($_SESSION["PALABRA"]==$_SESSION["BLANK"]){
+						echo '<br><br><label class="datos" >HAS GANADO</label>';
+						$myfile = fopen("Usuarios.txt", "r") or die("Unable to open file!");
+						$texto="";
+						$texto.=fgets($myfile);//saltar primer separador
+						while(!feof($myfile)){
+							$nombre=fgets($myfile);//conseguir nombre
+							$apellidos=fgets($myfile);//conseguir apppelidos
+							$texto.=$nombre;
+							$texto.=$apellidos;
+							if ($_GET["nom"]== trim($nombre)){
+								$_SESSION["Nombre"]=$nombre;
+								$_SESSION["Linea"]=$linea;
+								$texto.=fgets($myfile);//saltar correo
+								$texto.=fgets($myfile);//guardar fecha
+								$texto.=fgets($myfile);//saltar partidas jugadas
+								$texto.=((int)fgets($myfile)+1)."\n";//saltar partidas ganadas
+								$texto.=fgets($myfile);//saltar linea (separador)
+							}else{
+								$texto.=fgets($myfile);//saltar correo
+								$texto.=fgets($myfile);//guardar fecha
+								$texto.=fgets($myfile);//saltar partidas jugadas
+								$texto.=fgets($myfile);//saltar partidas ganadas
+								$texto.=fgets($myfile);//saltar linea (separador)
+								$linea=$linea+7;//guarda la line desde donde empieza el usuario
+							}
+			
+						}
+						fclose($myfile);
+						file_put_contents("Usuarios.txt", $texto);
+						$_POST["Comprobar"]="no";
+					}else{
+						echo '<br><br><label class="datos" >Letra:</label><select class="datos" name="Letra">';
 						for($i=97;$i<123;$i++)
 							echo '<option value="'.chr($i).'">'.chr($i).'</option>';
-					?>
-  				</select>
+	  					echo '</select>';
+					}
+				?>
 
 				<br><input id="lives" class="hide" type="number" name="Vida" <?php echo 'value='.((int)$_POST["Vida"]);?>>
-				<input class="hide" type="radio" name="Comprobar" checked><?php echo $_POST["Letra"];?>
-				<br><button type="submit">Enviar</button>
+				<input class="hide" type="radio" value="yes" name="Comprobar" checked>
+				<br><button type="submit" <?php 
+												if ((int)$_POST["Vida"]<=0 || $_POST["Comprobar"]=="no")
+													echo 'disabled';
+											?>>Enviar</button>
 			</fieldset>
 		</form>
 	</body>
